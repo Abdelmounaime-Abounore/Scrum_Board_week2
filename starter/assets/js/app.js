1dd0a8c3e50e3bf81bc916b88a8f9dddcbe46752
Abdelmounaime-Abounore <abdelmounaime.abounore@gmail.com>
2022-10-23 22:35:15 +0100

    brief terminé

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -2,44 +2,141 @@
  * In this file app.js you will find all CRUD functions name.
  * 
  */
-
-
-var todolist = document.getElementById("to-do-tasks");
-
-var title = document.getElementById('title');
-
-var priority = document.getElementById('priority');
-
-var statut = document.getElementById('statut');
-
-var date = document.getElementById('date');
-
-var description = document.getElementById('description');
-
-var save = document.getElementById('save');
-save.addEventListener('click', createTask);
+let globalvar;
+var form = document.getElementById("form");
+// form.addEventListener("submit",updateTask)
 
 // ------------------------------------------------
 
-function createTask() {
+function afficher() {
     // initialiser task form
     
     // Afficher le boutton save
 
     // Ouvrir modal form
 
-        var myobjet = {
-        Title       : title.value,
-        Type        : document.querySelector('.checks:checked').value,
-        Priority    : priority.value,
-        Statut      : statut.value, 
-        Date        : date.value,
-        Description : description.value,
-        }
-        tasks.push(myobjet)
-        // clearData();
-        saveTask();
+var para1 = document.getElementById('to-do-tasks-count');
+var counter1 = 0;
+var para2 = document.getElementById('in-progress-tasks-count');
+var counter2 = 0;
+var para3 = document.getElementById('done-tasks-count');
+var counter3 = 0;
+
+var todotasks = document.getElementById('to_do');
+todotasks.innerHTML = '';
+
+for(var i = 0; i < tasks.length; i++) {
+    if (tasks[i].statut == 'To Do') {
+        counter1++;
+        para1.innerHTML = counter1;  
+        todotasks.innerHTML += `
+       <button 
+       data-bs-toggle="modal" data-bs-target="#exampleModal"
+       onclick = editTask(${i})  class="p-2 d-flex text-start border w-100" style="background-color:#CAEBF2;" id="to_do">
+       <div class="col-md-1">
+           <i class="bi bi-question-circle"></i> 
+       </div>
+       <div class="col-md-11">
+           <div class="fw-bold">${tasks[i].title}</div>
+           <div class="">
+               <div class="text-success">#${i+1} created in ${tasks[i].date}</div>
+               <div class="" >${tasks[i].description}</div>
+           </div>
+           <div class="">
+               <span class="btn btn-info rounded-pill p-2">${tasks[i].priority}</span>
+               <span class="btn btn-gray-500 rounded-pill p-2">${tasks[i].type}</span>
+           </div>
+       </div>
+       </button>
+       `
     }
+}
+
+var inprogress = document.getElementById('in-progress-tasks');
+inprogress.innerHTML = '';
+
+
+for(i = 0; i < tasks.length; i++) {
+    if (tasks[i].statut == 'In Progress') {
+        counter2++;
+        para2.innerHTML = counter2; 
+        inprogress.innerHTML += `
+        <button 
+        data-bs-toggle="modal" data-bs-target="#exampleModal"
+        onclick = editTask(${i}) class="d-flex text-start border w-100" style="background-color:#CAEBF2;">
+        <div class="col-md-1">
+            <i class="spinner-border spinner-border-sm"></i> 
+        </div>
+        <div class="col-md-11">
+            <div class="fw-bold">${tasks[i].title}</div>
+            <div class="">
+                <div class="text-success">#${i+1} created in ${tasks[i].date}</div>
+                <div class="">${tasks[i].description}</div>
+            </div>
+            <div class="">
+                <span class="btn btn-info rounded-pill p-2">${tasks[i].priority}</span>
+                <span class="btn btn-gray-500 rounded-pill p-2">${tasks[i].type}</span>
+            </div>
+        </div>
+    </button>
+       `
+    }
+}
+
+var donetasks = document.getElementById('done-tasks');
+donetasks.innerHTML = '';
+
+
+for(i = 0; i < tasks.length; i++) {
+    if (tasks[i].statut == 'Done') {
+        counter3++;
+        para3.innerHTML = counter3; 
+        donetasks.innerHTML += `
+        <button 
+        data-bs-toggle="modal" data-bs-target="#exampleModal"
+        onclick = editTask(${i}) class="d-flex text-start border w-100" style="background-color:#CAEBF2;">
+        <div class="col-md-1">
+            <i class="bi bi-check-circle"></i> 
+        </div>
+        <div class="col-md-11">
+            <div class="fw-bold"">${tasks[i].title}</div>
+            <div class="">
+                <div class="text-success">${i+1} created in ${tasks[i].date}</div>
+                <div class="">${tasks[i].description}</div>
+            </div>
+            <div class="">
+                <span class="btn btn-info rounded-pill p-2">${tasks[i].priority}</span>
+                <span class="btn btn-gray-500 rounded-pill p-2">${tasks[i].type}</span>
+            </div>
+        </div>
+    </button>
+       `
+    }
+  }
+}
+
+var title = document.getElementById('title');
+var priority = document.getElementById('priority');
+var statut = document.getElementById('statut');
+var date = document.getElementById('date');
+var description = document.getElementById('description');
+
+function addTaks(){
+
+var myObject = {
+    title         : title.value,
+    type          : document.querySelector('.checks:checked').value,
+    priority      : priority.value,
+    statut        : statut.value,
+    date          : date.value,
+    description   : description.value,     
+}
+
+tasks.push(myObject);
+afficher();
+clearData();
+}
+
 
     function clearData() {
         title.value = '';
@@ -48,7 +145,19 @@ function createTask() {
         description.value = '';
     }
 
+    function checktypebtn(i)
+    {
+        if(tasks[i].type=='Feature')
+        
+        {
+            document.querySelector('#gridRadios1').checked = true ;
+        }
+        else
+        {
+            document.querySelector('#gridRadios2').checked = true ;
+        }
 
+    }
 function saveTask() {
     // Recuperer task attributes a partir les champs input
 
@@ -57,29 +166,14 @@ function saveTask() {
     // Ajoutez object au Array
 
     // refresh tasks
-    for (var i = 0; i < tasks.length; i++) {
-        if (todolist.statut.value === 'To Do') {
-        todolist.innerHTML += `<button class="p-2 d-flex text-start border w-100" style="background-color:#CAEBF2;" id="done">
-        <div class="col-md-1">
-            <i class="bi bi-question-circle"></i> 
-        </div>
-        <div class="col-md-11">
-            <div class="fw-bold">${tasks[i].title}</div>
-            <div class="">
-                <div class="text-success">#1 created in ${tasks[i].date}</div>
-                <div class=""> ${tasks[i].description} </div>
-            </div>
-            <div class="">
-                <span class="btn btn-info rounded-pill p-2">High</span>
-                <span class="btn btn-gray-500 rounded-pill p-2">Feature</span>
-            </div>
-        </div>
-    </button>
-    ` 
-    }    
+       
 }
 
-function editTask(index) {
+var update = document.getElementById('update');
+var suprimer = document.getElementById('delete');
+var save = document.getElementById('save');
+
+function editTask(i) {
     // Initialisez task form
 
     // Affichez updates
@@ -91,9 +185,30 @@ function editTask(index) {
     // Definir FORM INPUTS
 
     // Ouvrir Modal form
+    globalvar = i;
+    update.style.display = 'block';
+    suprimer.style.display = 'block';
+    save.style.display = 'none';
+
+    title.value = tasks[i].title;
+    priority.value = tasks[i].priority;
+    checktypebtn(i);
+    statut.value = tasks[i].statut;
+    date.value = tasks[i].date;
+    description.value = tasks[i].description;
 }
+function saveButon() {
+    save.style.display = 'block';
+    update.style.display = 'none';
+    suprimer.style.display = 'none';
+    clearData();
+}
+console.log(update);
+let closebtn = document.querySelector('.close');
 
 function updateTask() {
+    
+    // e.preventDefault() 
     // GET TASK ATTRIBUTES FROM INPUTS
 
     // Créez task object
@@ -103,8 +218,20 @@ function updateTask() {
     // Fermer Modal form
 
     // Refresh tasks
+     
+    tasks[globalvar].title = title.value;
+    tasks[globalvar].type = document.querySelector('.checks:checked').value;
+    tasks[globalvar].priority = priority.value;
+    tasks[globalvar].statut = statut.value;
+    tasks[globalvar].date = date.value;
+    tasks[globalvar].description = description.value;
+    
+   // closebtn.click();
+//    $('#exampleModal').modal('hide');
+    afficher();
     
 }
+
 
 function deleteTask() {
     // Get index of task in the array
@@ -114,6 +241,10 @@ function deleteTask() {
     // close modal form
 
     // refresh tasks
+    tasks.splice(globalvar,1);
+    // closebtn.click();
+
+    afficher()
 }
 
 function initTaskForm() {
@@ -124,7 +255,7 @@ function initTaskForm() {
 
 function reloadTasks() {
     // Remove tasks elements
-
+    
     // Set Task count
 }
-}
+
